0fa1e6a6e6b8d2f30f697690b260c2536fa16920
Nasekin <nasekinwhiteorblack@gmail.com>
2024-04-04 18:09:57 +0300

    add comp form and productlist

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useTelegram } from "../../hooks/useTelegram";
+import './ProductList.css'
+
+const ProductList = () => {
+    return (
+        <div className={'header'}>
+            ProductList
+        </div>
+    );
+};
+
+export default ProductList;
